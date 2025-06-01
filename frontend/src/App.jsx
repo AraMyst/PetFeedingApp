167751c622a12ae6af471bbdfd336e20690cbf01
Karla de Matos <kcristinardematos@gmail.com>
2025-06-01 15:45:32 +0200

    pets 9

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,30 @@
+// src/App.jsx
 import React from 'react'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext'
 import { NotificationProvider } from './contexts/NotificationContext'
 import PrivateRoute from './routes/PrivateRoute'
-
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
 import DashboardPage from './pages/DashboardPage'
-
 import FoodsPage from './pages/FoodsPage'
 import FoodCreatePage from './pages/FoodCreatePage'
 import FoodEditPage from './pages/FoodEditPage'
-
 import PetsPage from './pages/PetsPage'
 import NotificationsPage from './pages/NotificationsPage'
 import './index.css'
 
+/**
+ * App wraps the entire application in AuthProvider and NotificationProvider.
+ * It defines public and protected routes.
+ */
 export default function App() {
   return (
     <AuthProvider>
       <NotificationProvider>
         <BrowserRouter>
           <Routes>
-            {/* Redirect root “/” to login */}
+            {/* Redirect root (“/”) to login */}
             <Route path="/" element={<Navigate to="/login" replace />} />
 
             {/* Public routes */}
